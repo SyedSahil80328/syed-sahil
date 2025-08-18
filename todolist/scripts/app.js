eb220d0f6c301e245169a1bdebd0d1efdd1fc2fa
Syed Sahil <syedsahil28803@gmail.com>
2025-08-18 19:57:33 +0530

    Perform testing operations in file reading.

--- todolist/scripts/app.js
+++ todolist/scripts/app.js
@@ -1,14 +1,22 @@
 // Fetch and display tasks
-fetch('../assets/tasks.txt')
-  .then(response => response.text())
-  .then(text => {
-    const tasks = text.split('\n'); // Split by line
-    const list = document.getElementById('taskList');
 
-    tasks.forEach(task => {
-      const li = document.createElement('li');
-      li.textContent = task;
-      list.appendChild(li);
-    });
-  })
-  .catch(err => console.error('Error loading file:', err));
+let i=1;
+function readFile() {
+    i++;
+    fetch(`../assets/tasks-${i%2}.txt`)
+    .then(response => response.text())
+    .then(text => {
+      const tasks = text.split('\n'); // Split by line
+      const list = document.getElementById('taskList');
+      list.innerHTML = null;
+
+      tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.textContent = task;
+        list.appendChild(li);
+      });
+    })
+    .catch(err => console.error('Error loading file:', err));
+}
+
+setInterval(readFile, 5000);
